58416b88136384e955f6d377eb1f840e6f97fa7c
Andreas Raeder <andreas.raeder@isc.fraunhofer.de>
2023-10-07 12:13:38 +0000

    rm output prefix

--- src/maps/generic/GenericMapping.java
+++ src/maps/generic/GenericMapping.java
@@ -52,8 +52,8 @@ public class GenericMapping {
     public void run(String inputFileName) {
         // PGWriter instance_pgwriter = new YPGWriter("instance.ypg");
         // PGWriter schema_pgwriter = new YPGWriter("schema.ypg");
-        RapsqlCsvWriter instance_pgwriter = new RapsqlCsvWriter("gdm-instance.csv");
-        RapsqlCsvWriter schema_pgwriter = new RapsqlCsvWriter("gdm-schema.csv");
+        RapsqlCsvWriter instance_pgwriter = new RapsqlCsvWriter("instance.csv");
+        RapsqlCsvWriter schema_pgwriter = new RapsqlCsvWriter("schema.csv");
         this.run(inputFileName, instance_pgwriter, schema_pgwriter);
     }
 
